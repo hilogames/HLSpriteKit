215fe965b4de922bd3bd9749a81e623b4e759d0e
Karl Voskuil <offbyfour@gmail.com>
2015-11-30 09:20:07 -0500

    Update project settings and sundry for latest Xcode and SDK.

--- Example/HLSpriteKit/HLViewController.m
+++ Example/HLSpriteKit/HLViewController.m
@@ -37,7 +37,7 @@
   return YES;
 }
 
-- (NSUInteger)supportedInterfaceOrientations
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
 {
   if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
     return UIInterfaceOrientationMaskAllButUpsideDown;
