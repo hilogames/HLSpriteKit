a0428ca547e770373753f6b5cca1872e794eeed0
Karl Voskuil <offbyfour@gmail.com>
2016-09-21 10:47:30 -0400

    Update testing scripts to test iOS and macOS.

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e -o pipefail
+
+xcodebuild clean build test \
+           -workspace Example/HLSpriteKit.xcworkspace \
+           -scheme macOS \
+    | xcpretty
+
+# note: Would like to do test of OS=8.4, but xcodebuild hangs after simulator launch.
+# Just build it.
+xcodebuild clean build \
+           -workspace Example/HLSpriteKit.xcworkspace \
+           -scheme iOS \
+           -sdk iphonesimulator10.0 \
+           -destination 'platform=iOS Simulator,OS=8.4,name=iPhone 5s' \
+    | xcpretty
+
+xcodebuild clean build test \
+           -workspace Example/HLSpriteKit.xcworkspace \
+           -scheme iOS \
+           -sdk iphonesimulator10.0 \
+           -destination 'platform=iOS Simulator,OS=9.0,name=iPad 2' \
+    | xcpretty
+
+xcodebuild clean build test \
+           -workspace Example/HLSpriteKit.xcworkspace \
+           -scheme iOS \
+           -sdk iphonesimulator10.0 \
+           -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' \
+    | xcpretty
