5a411159b0dfaac1794c675efbe1da84a13c95ba
Karl Voskuil <offbyfour@gmail.com>
2016-09-21 12:16:05 -0400

    Trying a whole bunch of things to fix TravisCI configuration.

--- test.sh
+++ test.sh
@@ -3,11 +3,11 @@
 set -e -o pipefail
 
 xcodebuild clean build test \
-           CODE_SIGN_IDENTITY='' CODE_SIGNING_REQUIRED=NO \
            -workspace Example/HLSpriteKit.xcworkspace \
            -scheme macOS \
+           -sdk macosx10.11 \
            -destination 'platform=macOS' \
-    | xcpretty
+           ONLY_ACTIVE_ARCH=NO CODE_SIGN_IDENTITY='' CODE_SIGNING_REQUIRED=NO
 
 # note: Would like to do test of OS=8.4, but xcodebuild hangs after simulator launch.
 # Just build it.
@@ -16,7 +16,7 @@ xcodebuild clean build \
            -scheme iOS \
            -sdk iphonesimulator10.0 \
            -destination 'platform=iOS Simulator,OS=8.4,name=iPhone 5s' \
-    | xcpretty
+           ONLY_ACTIVE_ARCH=NO
 
 xcodebuild clean build test \
            -workspace Example/HLSpriteKit.xcworkspace \
@@ -24,4 +24,4 @@ xcodebuild clean build test \
            -sdk iphonesimulator10.0 \
            -destination 'platform=iOS Simulator,OS=9.0,name=iPad 2' \
            -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' \
-    | xcpretty
+           ONLY_ACTIVE_ARCH=NO
