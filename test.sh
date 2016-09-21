6276367a4a7aa39c7f3087ef93f95062f30adaf1
Karl Voskuil <offbyfour@gmail.com>
2016-09-21 11:37:24 -0400

    Try to fix macOS testing configuration on TravisCI.

--- test.sh
+++ test.sh
@@ -3,8 +3,10 @@
 set -e -o pipefail
 
 xcodebuild clean build test \
+           CODE_SIGN_IDENTITY='' CODE_SIGNING_REQUIRED=NO \
            -workspace Example/HLSpriteKit.xcworkspace \
            -scheme macOS \
+           -destination 'platform=macOS' \
     | xcpretty
 
 # note: Would like to do test of OS=8.4, but xcodebuild hangs after simulator launch.
@@ -21,11 +23,5 @@ xcodebuild clean build test \
            -scheme iOS \
            -sdk iphonesimulator10.0 \
            -destination 'platform=iOS Simulator,OS=9.0,name=iPad 2' \
-    | xcpretty
-
-xcodebuild clean build test \
-           -workspace Example/HLSpriteKit.xcworkspace \
-           -scheme iOS \
-           -sdk iphonesimulator10.0 \
            -destination 'platform=iOS Simulator,OS=latest,name=iPhone 6' \
     | xcpretty
