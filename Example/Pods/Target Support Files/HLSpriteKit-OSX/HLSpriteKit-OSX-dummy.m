ca7fd13cbf5c80d1842d0ffd971718ae0b440850
Karl Voskuil <offbyfour@gmail.com>
2018-06-12 16:21:22 -0400

    Update pods.

--- Example/Pods/Target Support Files/HLSpriteKit-OSX/HLSpriteKit-OSX-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HLSpriteKit_OSX : NSObject
-@end
-@implementation PodsDummy_HLSpriteKit_OSX
-@end
---
+++ Example/Pods/Target Support Files/HLSpriteKit-macOS/HLSpriteKit-macOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HLSpriteKit_macOS : NSObject
+@end
+@implementation PodsDummy_HLSpriteKit_macOS
+@end
